699e5ff55a850c5c9861fb3aabdf07087216e245
muhamistau <islam.taufikurahman@gmail.com>
2019-09-11 13:19:26 +0700

    Initial commit

---
+++ app/src/main/java/com/sawadikap/sawadikap/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sawadikap.sawadikap.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sawadikap.sawadikap.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
